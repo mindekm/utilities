d3dfcefe3e595e85e56a4d320d9e82d77d28b251
Milan Mindek <milan.mindek@gmail.com>
2019-02-09 23:49:19 +0100

    DoesNotContain

--- src/Utilities/Ensure/Extensions/EnsureStringExtensions.cs
+++ src/Utilities/Ensure/Extensions/EnsureStringExtensions.cs
@@ -1,6 +1,8 @@
 ﻿namespace Utilities.Extensions
 {
     using System;
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices.ComTypes;
 
     public static class EnsureStringExtensions
     {
@@ -25,6 +27,38 @@
             where TException : Exception
         {
 
+        }
+
+        public static void DoesNotContain<TException>(this in That<string> that, string target)
+            where TException : Exception
+        {
+            that.DoesNotContain(target, new EnsureException());
+        }
+
+        public static void DoesNotContain<TException>(this in That<string> that, string target, TException exception)
+            where TException : Exception
+        {
+            Guard.NotNull(exception, nameof(exception));
+
+            that.DoesNotContain(target, () => exception);
+        }
+
+        public static void DoesNotContain<TException>(this in That<string> that,
+            string target,
+            Func<TException> exceptionFactory)
+            where TException : Exception
+        {
+            Guard.NotNull(exceptionFactory, nameof(exceptionFactory));
+
+            that.DoesNotContain(target, _ => exceptionFactory());
+        }
+
+        public static void DoesNotContain<TException>(this in That<string> that, string target, Func<string, TException> exceptionFactory)
+            where TException : Exception
+        {
+            Guard.NotNull(exceptionFactory, nameof(exceptionFactory));
+
+            that.Fails(value => value.Contains(target), exceptionFactory);
         }
     }
 }
