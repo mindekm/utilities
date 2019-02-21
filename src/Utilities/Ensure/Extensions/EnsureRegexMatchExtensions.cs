0072d6ed9df15b68b5598f01015562ab92bb2f4a
Milan Mindek <milan.mindek@gmail.com>
2019-02-21 20:59:14 +0100

    Ensure for Regex Group

--- src/Utilities/Ensure/Extensions/EnsureRegexMatchExtensions.cs
+++ src/Utilities/Ensure/Extensions/EnsureRegexMatchExtensions.cs
@@ -17,9 +17,33 @@
             }
         }
 
+        public static void IsSuccess(this in That<Group> that) => that.IsSuccess(Error.EnsureFailure());
+
+        public static void IsSuccess(this in That<Group> that, Exception exception)
+        {
+            Guard.NotNull(exception, nameof(exception));
+
+            if (!that.Item.Success)
+            {
+                throw exception;
+            }
+        }
+
         public static void IsFailure(this in That<Match> that) => that.IsFailure(Error.EnsureFailure());
 
         public static void IsFailure(this in That<Match> that, Exception exception)
+        {
+            Guard.NotNull(exception, nameof(exception));
+
+            if (that.Item.Success)
+            {
+                throw exception;
+            }
+        }
+
+        public static void IsFailure(this in That<Group> that) => that.IsFailure(Error.EnsureFailure());
+
+        public static void IsFailure(this in That<Group> that, Exception exception)
         {
             Guard.NotNull(exception, nameof(exception));
 
