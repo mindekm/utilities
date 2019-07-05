cb06dd72fd20480eed6996d9e2d81df32118db13
Milan Mindek <milan.mindek@gmail.com>
2019-07-05 17:22:14 +0200

    Add string extensions

---
+++ src/Utilities/Extensions/StringExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace Utilities.Extensions
+{
+    using System;
+    using System.Linq;
+
+    public static class StringExtensions
+    {
+        private static readonly string[] TrueValues = { "true", "yes", "1" };
+
+        private static readonly string[] FalseValues = { "false", "no", "0" };
+
+        public static bool IsNullOrEmpty(this string item) => string.IsNullOrEmpty(item);
+
+        public static bool IsNullOrWhiteSpace(this string item) => string.IsNullOrWhiteSpace(item);
+
+        public static bool TryConvertToBool(this string item, out bool result)
+        {
+            Guard.NotNull(item, nameof(item));
+
+            if (TrueValues.Contains(item, StringComparer.OrdinalIgnoreCase))
+            {
+                result = true;
+                return true;
+            }
+
+            if (FalseValues.Contains(item, StringComparer.OrdinalIgnoreCase))
+            {
+                result = false;
+                return true;
+            }
+
+            result = default;
+            return false;
+        }
+    }
+}
