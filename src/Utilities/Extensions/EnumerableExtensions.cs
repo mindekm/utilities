1f31544967d67b44aac62a4b568175bfb463cc91
Milan Mindek <milan.mindek@gmail.com>
2019-08-24 19:10:29 +0200

    Add Enumerable extensions

---
+++ src/Utilities/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace Utilities.Extensions
+{
+    using System.Collections.Generic;
+
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<(T, int)> EnumerateWithIndex<T>(this IEnumerable<T> source)
+        {
+            var index = -1;
+
+            foreach (var item in source)
+            {
+                checked
+                {
+                    index++;
+                }
+
+                yield return (item, index);
+            }
+        }
+
+        public static IEnumerable<(T, int)> EnumerateWithProgress<T>(this IEnumerable<T> source)
+        {
+            var index = 0;
+
+            foreach (var item in source)
+            {
+                checked
+                {
+                    index++;
+                }
+
+                yield return (item, index);
+            }
+        }
+    }
+}
